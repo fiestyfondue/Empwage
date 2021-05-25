6924b1a02f73228d49ccec0d06f52b6db37488dc
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-05-25 08:15:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Employee_Check
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Employee wage computation ");
+            EmpWage EmpWage = new EmpWage();
+            EmpWage.Attendence();
+
+        }
+    }
+}
